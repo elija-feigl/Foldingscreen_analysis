f2b415f225e5bcda82651d9a43a3ff36a698c7d7
kourosh-zargari <krshz247@outlook.com>
2021-06-16 10:22:45 +0200

    Refactor: * add profiles and fullprofiles to gel species * close all plots at the end

--- foldingscreen_analysis/ifs/private/compute_profiles.m
+++ foldingscreen_analysis/ifs/private/compute_profiles.m
@@ -1,4 +1,4 @@
-function [gelData, gelInfo, profileData] = compute_profiles(pname, name, txt_file, img_file)
+function [profileData, gel, gelInfo] = compute_profiles(pname, name, txt_file, img_file)
 % @ step1
 % calculate profiles from gels
 
@@ -7,25 +7,43 @@ function [gelData, gelInfo, profileData] = compute_profiles(pname, name, txt_fil
     check_parsed_gel_info(gelInfo);
 
     %% via MATLAB_TOOLBOX
-    % load gelData
+    % load gel
     gelData_raw = load_gel_image('data_dir', pname, 'n_images', 1, 'paths_to_images', {img_file});
     % check and correct raw data
     gelData_raw = check_gel_saturation(gelData_raw);
-    gelData = background_correct_gel_image(gelData_raw, 'histogram_background', 'on');
+    gel = background_correct_gel_image(gelData_raw, 'histogram_background', 'on');
     % NOTE: in rare cases the values below zero still apear
     % TODO: pocket correction would be nice
     % get profileData
-    profileData = get_gel_lanes(gelData, 'display', 'on', 'cutoff', 0.05, ...
+    profileData = get_gel_lanes(gel, 'display', 'on', 'cutoff', 0.05, ...
         'number_of_lanes', length(gelInfo.lanes), 'display', 'off', 'move_min_zero', 'Yes');
 
+    % adding ladder and scaffold lane profiles to their species
+      
+    ladder_index = gelInfo.species.ladder.indices;
+    gelInfo.species.ladder.fullprofiles = profileData.fullProfiles(ladder_index);
+    gelInfo.species.ladder.profiles = profileData.profiles(ladder_index);
     
-    %% check and link data
-    % TODO move print figure
+    scaffold_index = gelInfo.species.scaffold.indices;
+    gelInfo.species.scaffold.fullprofiles = profileData.fullProfiles(scaffold_index);
+    gelInfo.species.scaffold.profiles = profileData.profiles(scaffold_index);
+    
+    idx = gelInfo.species.mono.indices;
+    gelInfo.species.mono.fullprofiles = profileData.fullProfiles(idx);
+    gelInfo.species.mono.profiles = profileData.profiles(idx);
+    
+    % bounding box of each lane
+    gelInfo.lane_bounding_box = profileData.lanePositions;
+    
+    %% FIgure
+    % TODO: move print figure
+    
     if length(profileData.fullProfiles)==length(gelInfo.lanes)
         n = length(profileData.profiles);
+        
         % plot areas 
         cur_fig = figure('Visible','on', 'PaperPositionMode', 'manual','PaperUnits','points','PaperPosition', [0 0 1000 500], 'Position', [0 1000 1000 500]);
-        imagesc(gelData.images{1}, [0 3.*std(gelData.images{1}(:))]), axis image, colormap gray, hold on
+        imagesc(gel.images{1}, [0 3.*std(gel.images{1}(:))]), axis image, colormap gray, hold on
 
         areas = [profileData.lanePositions(:,1)   profileData.lanePositions(:,3) profileData.lanePositions(:,2)-profileData.lanePositions(:,1)  profileData.lanePositions(:,4)-profileData.lanePositions(:,3)];
 
@@ -55,6 +73,8 @@ function [gelData, gelInfo, profileData] = compute_profiles(pname, name, txt_fil
     else
         disp('Warning: number of lanes in gel_info.txt does noth match number of detected lanes.' )
     end
+    
+    close all
 
 
 end
