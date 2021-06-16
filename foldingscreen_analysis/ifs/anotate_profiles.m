0bfcf03e3c7d1ffb26e6f4d34363d4d8f6dd9bb1
kourosh-zargari <krshz247@outlook.com>
2021-06-16 10:21:15 +0200

    Refactor: * new conditions to run code at step 2 (check for redo step 2 ) * remove saving fractions data to a file (irrelevant)

--- foldingscreen_analysis/ifs/anotate_profiles.m
+++ foldingscreen_analysis/ifs/anotate_profiles.m
@@ -1,29 +1,24 @@
-function anotate_profiles(name, pname, sigma_integrate)
+function data = anotate_profiles(name, pname, data)
 % step2: IFS analysis pipeline
 % anotate the profiles by marking species
     fname = [name  '_data.mat'];
-    data = load([pname filesep fname]); % load data
-    % select aggregates and monomer bands
-    data.gelInfo.sigma_integrate = sigma_integrate;
+ 
+    try
+        data.gelInfo.peaks_ok
+    catch
+        data.gelInfo.peaks_ok = false;      
+    end
 
-    [data.profileData, data.gelInfo] = select_species(data.profileData, data.gelData, data.gelInfo);
+    % select aggregates and monomer bands
+    [data.profileData, data.gelInfo] = select_species(data.profileData, data.gelData, data.gelInfo); %TODO: delete profileData
 
 
     % integrate aggregates, smear, and monomer bands
-    data.profileData = integrate_species(data.profileData, data.gelInfo);
+    [data.profileData, data.gelInfo] = integrate_species(data. profileData, data.gelInfo); %TODO: delete profileData
     
     % save
     disp(['Saving to ' pname filesep fname]);
     save([pname filesep fname], '-struct', 'data');
-    
-    fname_fractions = [name '_fractions.out'];
-    disp(['Saving fractions to ' pname filesep fname_fractions])
-    fid = fopen([pname filesep fname_fractions],'w'); 
-    fprintf(fid,'%s\n','#Monomer Smear Pocket MonomerFraction SmearFraction PocketFraction');
-    fclose(fid);
-    tmp = [data.profileData.monomerTotal data.profileData.smearTotal data.profileData.pocketTotal];
-    fraction_tmp = tmp./(tmp(:,1)+tmp(:,2)+tmp(:,3));
-    dlmwrite([pname filesep fname_fractions], [tmp fraction_tmp], 'delimiter', '\t', '-append');
     disp('Done')
 
 end
