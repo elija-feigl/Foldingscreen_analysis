989fa7c2cf6266226ab0ee2c982fdeb7f4d56729
kourosh-zargari <krshz247@outlook.com>
2021-06-16 10:14:12 +0200

    Refactor: * Pass data through each step, avoid loading each time

--- foldingscreen_analysis/IFS_analysis.m
+++ foldingscreen_analysis/IFS_analysis.m
@@ -3,9 +3,14 @@ folder = fileparts(mfilename('fullpath'));
 addpath(genpath(folder));
 
 %% execute this to select a folder and analyze a folding screen
-[pname] = uigetdir(pwd, 'Select an Directory with intial foling screen');
+[pname] = uigetdir(pwd, 'Select an Directory with intial folding screen');
 sigma_integrate_band = 1.0;
 
-[name] = make_profiles(pname);
-anotate_profiles(name, pname, sigma_integrate_band);
-analyse_profiles(name, pname);
+% Step 1
+[name, data] = make_profiles(pname);
+
+% Step 2
+data = anotate_profiles(name, pname, data);
+
+% Step 3
+analyse_profiles(name, pname, data);
