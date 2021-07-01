20616f6f0c965e433bfa9a56864b6c1eb6f8c7f2
kourosh-zargari <krshz247@outlook.com>
2021-07-01 22:21:27 +0200

    Refactor: * removed profileData dependancy * species.select_position: selected point on gel by user * added normalization of band migration distance to the fitting process

--- foldingscreen_analysis/ifs/private/species_fits.m
+++ foldingscreen_analysis/ifs/private/species_fits.m
@@ -1,10 +1,9 @@
         
 %% Calculate fits
-function [profileData, gelInfo] = species_fits(profileData, gelInfo, gelData) %TODO: delete profileData
-
+function gelInfo = species_fits(gelInfo, gelData)
     %% Get band fits
     function fits = get_band_fit(species, pos)
-        xpos = species.positions(pos) - fix(species.height/2);
+        xpos = species.select_positions(pos) - fix(species.height/2);
         fits = zeros(1, 3);
         
         upper = xpos + species.height;
@@ -30,38 +29,25 @@ function [profileData, gelInfo] = species_fits(profileData, gelInfo, gelData) %T
         elseif species.type == "scaffold"
             fits = zeros(1,3);
         else
-            xpos = species.positions(pos) - fix(height/2);
-            upper = xpos + height;
-            val = species.fullprofiles{pos};   
+            xpos = species.staple.select_positions(pos) - fix(species.height/2);
+            if xpos < pocket_position
+                fits = [0.0 pocket_position 0.0];
+            else
+                upper = xpos + height;
+                val = species.fullprofiles{pos};   
 
-            if upper > length(species.fullprofiles{pos})
-                upper = length(species.fullprofiles{pos});
+                if upper > length(val)
+                    upper = length(val);
+                end
+
+                y = val(xpos:upper);
+                x = double(xpos:upper);
+                options = fitoptions('gauss1', 'Lower', [0 xpos 0], 'Upper', [Inf upper Inf]);
+
+                fits = coeffvalues(fit(x', y, 'gauss1', options));
             end
-
-            y = val(xpos:upper);
-            x = double(xpos:upper);
-            options = fitoptions('gauss1', 'Lower', [0 xpos 0], 'Upper', [Inf upper Inf]);
-
-            fits = coeffvalues(fit(x', y, 'gauss1', options));
         end
     end
-    
-    %TODO: delete after adapting part 3
-    mono_index = gelInfo.species.mono.indices;
-    nLanes_mono =  length(mono_index); 
-    
-    %TODO: delete after adapting part 3
-    scaffold_index = gelInfo.species.scaffold.indices;
-    nLanes_scaffold = length(scaffold_index); 
-    
-    %TODO: delete after adapting part 3
-    ladder_index = gelInfo.species.ladder.indices;
-    nLanes_ladder = length(ladder_index);  
-    
-    %TODO: delete fits, after adapting part 3
-    gelInfo.species.ladder.fits = zeros(nLanes_ladder, 3);
-    gelInfo.species.scaffold.fits = zeros(nLanes_scaffold, 3);
-    gelInfo.species.mono.fits = zeros(nLanes_mono, 3);
 
     % TODO: hardcoding these might not be ideal, as pixelsize can depend
     %       on the scanner used. find better solution!
@@ -78,6 +64,7 @@ function [profileData, gelInfo] = species_fits(profileData, gelInfo, gelData) %T
     pocket_position = double(gelInfo.pocket.bounding_box(2));
     height = gelInfo.pocket.bounding_box(4);
     pocket_y = zeros(height+1,1);
+    gelInfo.max_staple_migrate = 0 ;
     
     for i = 1:length(gelInfo.loop_indices)
     % starting with mono and then scaffold and ladder
@@ -85,23 +72,15 @@ function [profileData, gelInfo] = species_fits(profileData, gelInfo, gelData) %T
     
         indices = fliplr(gelInfo.loop_indices); 
         idx = indices(i);
-        [species,  pos] = get_lanes_from_idx(gelInfo, idx);
+        [spec,  pos] = get_lanes_from_idx(gelInfo, idx);
         
         % Pocket fit
-        pocket_y = pocket_y + species.fullprofiles{pos}(pocket_position : pocket_position+height);
+        pocket_y = pocket_y + spec.fullprofiles{pos}(pocket_position : pocket_position+height);
         
-        % Species fit
-        band_fit = get_band_fit(species,  pos);
-        gelInfo.species.(species.type).fits(pos, :) = band_fit; %TODO: delete after adapting part 3
-        gelInfo.species.(species.type).positions(pos) = band_fit(2)';
-        gelInfo.species.(species.type).band_width(pos) = band_fit(3)';
-        gelInfo.species.(species.type).migration_distance(pos) = band_fit(2)' - pocket_position';
-        
-        % For staples
-        staple_fit = get_staple_fit(species, pos, stapleHeight);
-        gelInfo.species.(species.type).staple.fits(pos, :) = staple_fit; %TODO: delete after adapting part 3
+        % staples fits
+        staple_fit = get_staple_fit(spec, pos, stapleHeight);
+        gelInfo.species.(spec.type).staple.fits(pos, :) = staple_fit;
   
-        
         % Ladder and Scaffold staple extrapolation from monomere staple migration
         try
             ends = gelInfo.species.mono.staple.migration_distance([1, end]);
@@ -110,17 +89,31 @@ function [profileData, gelInfo] = species_fits(profileData, gelInfo, gelData) %T
         catch
         end
         
-        if species.type == "ladder"
+        if spec.type == "ladder"
             gelInfo.species.ladder.staple.migration_distance(pos) = ends(pos) - 2*mono_staple_migrate(pos);
             
-        elseif species.type == "scaffold"
+        elseif spec.type == "scaffold"
             gelInfo.species.scaffold.staple.migration_distance(pos) = ends(pos) - mono_staple_migrate(pos);
             
         else
             gelInfo.species.mono.staple.migration_distance(pos) = staple_fit(2) - pocket_position;
         end
         
+        
+        % Species band fits
+        band_fit = get_band_fit(spec,  pos);
+        gelInfo.species.(spec.type).fits(pos, :) = band_fit;
+        gelInfo.species.(spec.type).band_positions(pos) = band_fit(2)';
+        gelInfo.species.(spec.type).band_width(pos) = band_fit(3)';
+        migration_distance = band_fit(2)' - pocket_position';
+        
+        % Migration distance
+        gelInfo.species.(spec.type).migration_distance(pos) = migration_distance;
+        
     end
+    
+    % Normalized Migration distance and band spread
+    gelInfo = normalization(gelInfo);
   
     % Pocket fit
     pocket_x = double(pocket_position:pocket_position+height);
@@ -129,30 +122,6 @@ function [profileData, gelInfo] = species_fits(profileData, gelInfo, gelData) %T
     gelInfo.pocket.position = pocket_fits(2);
     gelInfo.pocket.width = pocket_fits(3);
     
-    %% get band data
-
-    % TODO: delete after adapting part 3
-    mono_fits = zeros(nLanes_ladder+nLanes_mono+nLanes_scaffold, 3);
-    
-    if ~isempty(gelInfo.species.ladder.indices)
-        mono_fits(gelInfo.species.ladder.indices, :) = gelInfo.species.ladder.fits;
-    end
-
-    if ~isempty(gelInfo.species.scaffold.indices)   
-        mono_fits(gelInfo.species.scaffold.indices, :) = gelInfo.species.scaffold.fits;
-    end
-
-    mono_fits(gelInfo.species.mono.indices, :) = gelInfo.species.mono.fits;
-
-
-    staples_fits = zeros(nLanes_ladder + nLanes_mono+ nLanes_scaffold, 3);
-    staples_fits(gelInfo.species.mono.indices, :) = gelInfo.species.mono.staple.fits;
-
-    %% add to profiles structure
-    % NOTE: output necessary for legacy python code(ifs database code) 
-    % TODO: delete after adapting part 3
-    profileData.monomerFits = mono_fits; % ladder, scaffold and monomers fits
-    profileData.stapleFits = staples_fits;  
 
     %% Display results and ask if ok
     close all
