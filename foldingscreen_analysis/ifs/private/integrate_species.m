b3cdeace16a0eac2782c565c4f6d09918572007c
kourosh-zargari <krshz247@outlook.com>
2021-07-01 22:11:55 +0200

    Refactor: * replaced new data structure from gelInfo * save fraction_pocket to gelInfo for plot analysis * save fraction_monomer to gelInfo for plot analysis * save fraction_smear to gelInfo for plot analysis

--- foldingscreen_analysis/ifs/private/integrate_species.m
+++ foldingscreen_analysis/ifs/private/integrate_species.m
@@ -1,62 +1,52 @@
-function [profileData, gelInfo] = integrate_species(profileData, gelInfo) %TODO: delete profileData
+function gelInfo = integrate_species(gelInfo) 
 % @ step2
 % compute fractions of selected species per lane
 
     % integrate aggregates, monomers and smear
     n = length(gelInfo.loop_indices);
-    
-    %TODO: delete after adapting part 3
-    profileData.monomerTotal = zeros(n, 1);
-    profileData.pocketTotal = zeros(n, 1);
-    profileData.smearTotal = zeros(n, 1);
-    
+
     
     mu = gelInfo.pocket.position;
     sig = gelInfo.pocket.width * gelInfo.sigma_integrate;
 
     for i = 1:n
         idx = gelInfo.loop_indices(i);
-        [species,  pos] = get_lanes_from_idx(gelInfo, idx);
+        [spec,  pos] = get_lanes_from_idx(gelInfo, idx);
         
-        if species.type == "ladder" || species.type == "scaffold"
+        if spec.type == "ladder" || spec.type == "scaffold"
             % integrate aggregates
-            gelInfo.species.(species.type).pocket_sums(pos) = 1.0;
-            profileData.pocket_sums(idx) = 1.0; %TODO: delete after adapting part 3
+            pocket_sums = 1.0;
             
             % integrate monomers
-            gelInfo.species.(species.type).monomer_sums(pos) = 0.0;
-            profileData.monomer_sums(idx) = 0.0; %TODO: delete after adapting part 3
+            monomer_sums = 0.0;
             
             % intergrate smear
-            gelInfo.species.(species.type).smear_sums(pos) = 0.0;
-            profileData.smear_sums(idx) = 0.0; %TODO: delete after adapting part 3
+            smear_sums = 0.0;
         else
             % integrate aggregates
             pocket_boundaries = [max(1,round(mu-sig)) round(mu+sig)];
-            pocket_sums = sum(species.fullprofiles{pos}(pocket_boundaries(1):pocket_boundaries(2) ));
-            gelInfo.species.(species.type).pocket_sums(pos) = pocket_sums;
-            profileData.pocket_sums(idx) = pocket_sums; %TODO: delete after adapting part 3
-            
+            pocket_sums = sum(spec.fullprofiles{pos}(pocket_boundaries(1):pocket_boundaries(2) ));
             
             % integrate monomers
-            mu_mono = species.positions(pos);
-            sig_mono = species.band_width(pos) * gelInfo.sigma_integrate;
+            mu_mono = spec.band_positions(pos);
+            sig_mono = spec.band_width(pos) * gelInfo.sigma_integrate;
             monomer_boundaries = [max(1,round(mu_mono-sig_mono)) round(mu_mono+sig_mono)];
-            monomer_sums = sum(species.fullprofiles{pos}(monomer_boundaries(1):monomer_boundaries(2)));
-            gelInfo.species.(species.type).monomer_sums(pos) = monomer_sums;
-            profileData.monomer_sums(idx) = monomer_sums; %TODO: delete after adapting part 3
+            monomer_sums = sum(spec.fullprofiles{pos}(monomer_boundaries(1):monomer_boundaries(2)));
             
             % intergrate smear
             smear_boundaries = [pocket_boundaries(2) monomer_boundaries(1)];
-            smear_sums = sum(species.fullprofiles{pos}(smear_boundaries(1):smear_boundaries(2)));
-            gelInfo.species.(species.type).smear_sums(pos) = smear_sums;
-            profileData.smear_sums(idx) = smear_sums; %TODO: delete after adapting part 3
+            smear_sums = sum(spec.fullprofiles{pos}(smear_boundaries(1):smear_boundaries(2)));
         end
-    end
-    
-    
-    
+            
+        % Total bands
+        total_band = smear_sums + monomer_sums + pocket_sums;
+            
+        % Fractions
+        gelInfo.species.(spec.type).fraction_pocket(pos) = pocket_sums ./ total_band ;
+        gelInfo.species.(spec.type).fraction_monomer(pos) = monomer_sums ./ total_band ;
+        gelInfo.species.(spec.type).fraction_smear(pos) = smear_sums ./ total_band ;
 
-    
+    end
+   
 end
 
